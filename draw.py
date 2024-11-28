a924ca183c8873906a0253b624e29a652f70d2b9
aroncal02 <154700667+ronmaccms@users.noreply.github.com>
2024-11-28 13:59:09 -0500

    tests

---
+++ draw.py
@@ -0,0 +1,33 @@
+import cv2 as cv
+import numpy as np
+
+# blank = np.zeros((500,500), dtype='uint8')
+# cv.imshow('Black', blank)
+
+# # 1. Paint the image a certain colour
+# blank[:] = 0,255,0
+# cv.imshow('Green', blank)
+
+# cv.waitKey(0)
+
+# PAINT 
+blank = np.zeros((500,500,3), dtype='uint8')
+cv.imshow('Black', blank)
+
+# # 1. Paint the image a certain colour
+# blank[200:300, 300:400] = 0,255,0
+# cv.imshow('Green', blank)
+
+# DRAW A RECTANGLE
+# cv.rectangle(blank, (0,0), (250,250), (0,250,0), thickness=2)
+# cv.rectangle(blank, (0,0), (250,500), (0,250,0), thickness=-1)
+# cv.rectangle(blank, (0,0), (250,500), (0,250,0), thickness=cv.FILLED)
+cv.rectangle(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2), (0,250,0), thickness=-1)
+cv.imshow('Rectangle', blank)
+
+#DRAW A CIRCLE
+cv.circle(blank, (blank.shape[1]//2, blank.shape[0]//2), 40, (0,0,255), thickness=3)
+cv.imshow('Circle', blank)
+
+
+cv.waitKey(0)
